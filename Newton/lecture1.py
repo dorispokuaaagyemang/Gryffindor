35204eb195b2fd33009e0d55e5d541b6beacf417
NewtonOduro <odurocollins345@gmail.com>
2026-02-17 11:11:42 +0000

    Newton: Home study

---
+++ Newton/lecture1.py
@@ -0,0 +1,10 @@
+# Using the built in function def
+def focus_on_yourself():
+    sentance = ("Focus on yourself and your own growth.")
+    print(sentance)
+
+# Using the input function to get user input
+name = input("Fuck bitches: ")
+
+
+focus_on_yourself()
